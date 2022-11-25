51c048ed86861182de6c78994c64e0f162fa9fc5
FelixLuciano <felix.lucianodias@gmail.com>
2022-11-25 09:55:26 -0300

    feat: add solvers

--- lib/__init__.py
+++ lib/__init__.py
@@ -3,5 +3,6 @@ from .beam import Beam
 from .truss import Truss
 from .force import Force
 from .block import Block
+from .solver import Solver, GaussSeidel_Solver
 from .profe import plota, importa, geraSaida
 from .load import Loads
